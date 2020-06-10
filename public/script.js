f9a67ac193d100db3ffba56f03c7b8ca29d94267
Glitch (temp-chat) <none>
2020-06-10 09:14:32 +0000

    📕😇 Updated with Glitch

--- public/script.js
+++ public/script.js
@@ -8,49 +8,110 @@ const name = prompt("Input your username: ");
 const channel = prompt(
   "Input your channel name: (leave empty for global channel)"
 );
-
+const zeroPad = (num, places) => String(num).padStart(places, "0");
 
 socket.emit("fetchinfo", channel);
 socket.on("getinfo", text => {
   let msgs = text.split("\n");
   for (let i = 0; i < msgs.length - 1; i++) {
-    appendmessage(msgs[i]);
+    appendmessage(
+      msgs[i].slice(0, msgs[i].length - 20),
+      msgs[i].slice(msgs[i].length - 20, msgs[i].length)
+    );
   }
   window.scrollTo(0, document.body.scrollHeight);
 });
-appendmessage("you joined");
+function gettime() {
+  let d = new Date();
+  return (
+    zeroPad(d.getDate().toString(), 2) +
+    "/" +
+    zeroPad(d.getMonth().toString(), 2) +
+    "/" +
+    d.getFullYear().toString() +
+    "  " +
+    zeroPad(d.getHours(), 2) +
+    ":" +
+    zeroPad(d.getMinutes(), 2) +
+    ":" +
+    zeroPad(d.getSeconds(), 2)
+  );
+}
+appendmessage("you joined", gettime());
 socket.emit("new-user", { name: name, channel: channel });
 
 socket.on("chat-message", data => {
   console.log(data);
   if (data.channel != channel) return;
-  appendmessage(`${data.name} : ${data.message}`);
+  let idx = data.message.length - 20;
+  let msg = data.message.slice(0, idx);
+  let ti = data.message.slice(idx, data.message.length);
+  appendmessage(`${data.name} : ${msg}`, ti);
 });
 socket.on("user-connected", data => {
   if (data.channel != channel) return;
-  appendmessage(`${data.name} connected`);
+  var d = new Date();
+  appendmessage(
+    `${data.name} connected`,
+    zeroPad(d.getDate().toString(), 2) +
+      "/" +
+      zeroPad(d.getMonth().toString(), 2) +
+      "/" +
+      d.getFullYear().toString() +
+      "  " +
+      zeroPad((d.getHours() + 8) % 24, 2) +
+      ":" +
+      zeroPad(d.getMinutes(), 2) +
+      ":" +
+      zeroPad(d.getSeconds(), 2)
+  );
 });
 
 socket.on("user-disconnected", data => {
   if (data.channel != channel) return;
-  appendmessage(`${data.name} disconnected`);
+  appendmessage(`${data.name} disconnected`, gettime());
 });
 
 messageform.addEventListener("submit", e => {
   console.log("submitted");
   e.preventDefault();
-  const message = messageinput.value;
-  appendmessage(`You: ${message}`);
+  let message = messageinput.value;
+  var d = new Date();
+  message +=
+    zeroPad(d.getDate().toString(), 2) +
+    "/" +
+    zeroPad(d.getMonth().toString(), 2) +
+    "/" +
+    d.getFullYear().toString() +
+    "  " +
+    zeroPad(d.getHours(), 2) +
+    ":" +
+    zeroPad(d.getMinutes(), 2) +
+    ":" +
+    zeroPad(d.getSeconds(), 2);
+  let idx = message.length - 20;
+  let msg = message.slice(0, idx);
+  let ti = message.slice(idx, message.length);
+  appendmessage(
+    `You: ${message.slice(0, idx)}`,
+    message.slice(idx, message.length)
+  );
   socket.emit("send-chat-message", message);
   messageinput.value = "";
   window.scrollTo(0, document.body.scrollHeight);
 });
 
-function appendmessage(message) {
+function appendmessage(message, ti) {
   const messageelement = document.createElement("div");
   messageelement.className = "container";
   let pelement = document.createElement("p");
   pelement.innerText = message;
+  let tielement = document.createElement("p");
+  let spanelement = document.createElement("span");
+  spanelement.className = "time-right";
+  spanelement.innerText = ti;
+  tielement.appendChild(spanelement);
   messageelement.appendChild(pelement);
+  messageelement.appendChild(tielement);
   messagecontainer.append(messageelement);
 }
--- server.js
+++ server.js
@@ -5,16 +5,22 @@ var io = require("socket.io")(server);
 var port = process.env.PORT || 3000;
 var fs = require("fs");
 app.use(express.static("public"));
+const zeroPad = (num, places) => String(num).padStart(places, "0");
 server.listen(port, function() {
   console.log("Server listening at port %d", port);
 });
+
+function gettime(){
+  let d = new Date()
+  return zeroPad(d.getDate().toString(), 2) +"/" +zeroPad(d.getMonth().toString(), 2) +"/" +d.getFullYear().toString() +"  " +zeroPad((d.getHours() + 8) % 24, 2) +":" +zeroPad(d.getMinutes(), 2) +":" +zeroPad(d.getSeconds(), 2);
+}
 
 const username = {};
 const channels = {};
 io.on("connection", socket => {
   socket.on("fetchinfo", channel => {
-    fs.appendFileSync("r" + channel + ".txt", "");
-    io.to(socket.id).emit("getinfo", fs.readFileSync("r" + channel + ".txt", "utf8"))
+    fs.appendFileSync(__dirname + "/data/r" + channel + ".txt", "");
+    io.to(socket.id).emit("getinfo", fs.readFileSync(__dirname + "/data/r" + channel + ".txt", "utf8"))
   });
   socket.on("new-user", data => {
     username[socket.id] = data.name;
@@ -23,7 +29,7 @@ io.on("connection", socket => {
       name: data.name,
       channel: data.channel
     });
-    fs.appendFileSync("r" + data.channel + ".txt", data.name + " connected\n");
+    fs.appendFileSync(__dirname + "/data/r" + data.channel + ".txt", data.name + " connected" + gettime() + "\n");
   });
   socket.on("send-chat-message", message => {
     socket.broadcast.emit("chat-message", {
@@ -31,7 +37,7 @@ io.on("connection", socket => {
       name: username[socket.id],
       channel: channels[socket.id]
     });
-    fs.appendFileSync("r" + channels[socket.id] + ".txt", username[socket.id] + ": " + message + "\n");
+    fs.appendFileSync(__dirname + "/data/r" + channels[socket.id] + ".txt", username[socket.id] + ": " + message + "\n");
   });
   socket.on("disconnect", () => {
     socket.broadcast.emit("user-disconnected", {
@@ -39,8 +45,8 @@ io.on("connection", socket => {
       channel: channels[socket.id]
     });
     fs.appendFileSync(
-      "r" + channels[socket.id] + ".txt",
-      username[socket.id] + " disconnected\n"
+      __dirname + "/data/r" + channels[socket.id] + ".txt",
+      username[socket.id] + " disconnected" + gettime() + "\n"
     );
     delete username[socket.id];
     delete channels[socket.id];
